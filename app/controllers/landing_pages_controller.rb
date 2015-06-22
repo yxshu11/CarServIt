16da0b5968e32078c4df50600916bbc35d371eae
Mac Shu <mac_shu11@hotmail.com>
2015-06-22 13:27:35 +0800

    Created Slightly Dynamic Landing pages

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,13 +1,17 @@
 class LandingPagesController < ApplicationController
   def home
+
   end
 
   def help
+
   end
 
   def about
+
   end
 
   def contact
+  	
   end
 end
--- test/controllers/landing_pages_controller_test.rb
+++ test/controllers/landing_pages_controller_test.rb
@@ -1,24 +1,32 @@
 require 'test_helper'
 
 class LandingPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "CarServIt"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About Us | #{@base_title}"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact Us | #{@base_title}"
   end
 
 end
